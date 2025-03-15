81c36fba040982c79eec3e8899dc1efe1b867d68
sudheeraja18 <gurramsudheerraja@gmail.com>
2025-03-15 16:15:52 +0530

    conversation added

---
+++ 10_install_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error: User need to have super user permission to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing mysql.....FAILURE"
+        exit 1
+    else
+        echo "Installing mysql.....SUCCESS"
+else
+    echo "mysql package already ....INSTALLED"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing git.....FAILURE"
+        exit 1
+    else
+        echo "Installing git.....SUCCESS"
+else
+    echo "git package already ....INSTALLED"
+fi
+
