62f47572d3117d255646aa2c902ba0a6f0041f8a
sudheeraja18 <gurramsudheerraja@gmail.com>
2025-03-18 23:52:50 +0530

    conversation added

---
+++ 13_logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="var/log/shellscript-logs"
+LOG_FILE=$(echo $1 | cut -d "d" -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGS_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2..... $G SUCCESS"
+    fi
+}
+
+echo "Script executed at time : $TIMESTAMP" &>>$LOGS_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error: User need to have super user permission to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql &>>$LOGS_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOGS_FILE_NAME
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "mysql package already .... $Y INSTALLED"
+fi
+
+dnf list installed git &>>$LOGS_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOGS_FILE_NAME
+    VALIDATE $? "Installing Git"
+else
+    echo -e "git package already .... $Y INSTALLED"
+fi
