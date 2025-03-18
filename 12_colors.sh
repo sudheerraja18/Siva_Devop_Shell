6fe14cb0b5ed11e7519be4f6be402f5444872ae8
sudheeraja18 <gurramsudheerraja@gmail.com>
2025-03-18 22:31:33 +0530

    conversation added

---
+++ 12_colors.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2..... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error: User need to have super user permission to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "mysql package already .... $Y INSTALLED"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo -e "git package already .... $Y INSTALLED"
+fi
