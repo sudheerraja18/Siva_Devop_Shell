ce6ba4708e7fbaecf0f6fac89f2d3684daad4180
sudheeraja18 <gurramsudheerraja@gmail.com>
2025-03-25 22:33:24 +0530

    Shell scripts

---
+++ common.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FILE="$(echo $0 | cut -d "." -f1)"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGS_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2..... $G SUCCESS $N"
+    fi
+}
